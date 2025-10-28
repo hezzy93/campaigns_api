e62b3666caeb339ce76437387b9f7eceec359dfd
Hezekiah <ajomewohezekiah@gmail.com>
2025-10-28 07:27:35 +0100

    Add protected routes

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Campaign API", version="1.0")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users_Login")
 
-@app.post("/users_Login")
+@app.post("/users_Login", tags=["User"])
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
@@ -94,7 +94,7 @@ def get_campaign_by_id(campaign_id: UUID, db: Session = Depends(get_db)):
 
 # Endpoint to UPDATE campaigns
 @app.put("/api/campaigns/{campaign_id}", tags=["Campaign"])
-def update_campaign(campaign_id: UUID, payload: schemas.CampaignUpdate, db:Session= Depends(get_db)):
+def update_campaign(campaign_id: UUID, payload: schemas.CampaignUpdate, db:Session= Depends(get_db), current_user: schemas.TokenData = Depends(get_current_user)):
     updated_campaign = crud.update_campaign(
         db=db,
         campaign_id=campaign_id,
@@ -107,7 +107,7 @@ def update_campaign(campaign_id: UUID, payload: schemas.CampaignUpdate, db:Sessi
 
 # Endpoint to DELETE a Campaign by Id
 @app.delete("/api/campaigns/{campaign_id}", tags=["Campaign"])
-def delete_campaign(campaign_id:UUID, db: Session = Depends(get_db)):
+def delete_campaign(campaign_id:UUID, current_user: schemas.TokenData = Depends(get_current_user), db: Session = Depends(get_db)):
     result = crud.delete_campaign(db,campaign_id)
 
     if "error" in result:
